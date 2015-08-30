a73557c6d2031d98537a7064556069ba759bcf6f
Albert De Guzman <adeguzman@launchbrigade.com>
2015-08-30 15:38:47 -0700

    removed the unused columns

--- tournament.sql
+++ tournament.sql
@@ -11,24 +11,14 @@ CREATE DATABASE tournament;
 \connect tournament;
 
 DROP TABLE IF EXISTS players CASCADE;
-DROP TABLE IF EXISTS player_statistics CASCADE;
 DROP TABLE IF EXISTS matches CASCADE;
 
 -- id, name
 CREATE TABLE players (name TEXT,
 											id SERIAL );
 
---This table is probably not needed
---player_id, win, lose
-CREATE TABLE player_statistics (id smallint,
-															 win smallint,
-															 lose smallint );
-
---first_player_id, second_player_id, winner, depth
+--id, winner, loser
 CREATE TABLE matches (id SERIAL,
 											winner smallint,
-											loser smallint,
-											round smallint );
---you could possible get the winner through player_statistic 
-
---CREATE TABLE tournaments
+											loser smallint
+											);
