aa0a92f5f119349777334726d6057a0d0cc72bff
Albert De Guzman <adeguzman@launchbrigade.com>
2015-08-30 15:54:36 -0700

    Removed unused functions

--- tournament.py
+++ tournament.py
@@ -2,8 +2,6 @@
 """tournament.py -- implementation of a Swiss-system tournament"""
 
 import psycopg2
-import math
-import random
 
 
 def connect():
@@ -59,18 +57,6 @@ def register_player(name):
     conn.close()
 
 
-def initialize_players():
-    """Adds players to the game"""
-    register_player('albert')
-    register_player('chris')
-    register_player('mike')
-    register_player('dylan')
-    register_player('hylan')
-    register_player('lauren')
-    register_player('jazz')
-    register_player('judith')
-
-
 def player_standings():
     """Returns a list of the players and their win records, sorted by wins.
 
@@ -115,6 +101,7 @@ def player_standings():
 
     return query_results
 
+
 def report_match(winner, loser):
     """Records the outcome of a single match between two players.
 
@@ -135,7 +122,6 @@ def swiss_pairings():
     """Swiss pairing is a sorting algorithm that allows players to be matched
     for the next round"""
 
-    paired_players = []
     """Returns a list of pairs of players for the next round of a match.
 
     Assuming that there are an even number of players registered, each player
@@ -151,19 +137,6 @@ def swiss_pairings():
         name2: the second player's name
     """
 
-    """
-        get a database connection
-        have it sort the players by the most wins
-        have it return a list of players
-    """
-
-    """needs to return a list of tuples
-    [(id1, name1, id2, name2),
-     (id2, name2, id2, name2),
-     ...,
-     (id2, name2, id2, name2)
-    ]
-    """
     conn = connect()
     cur = conn.cursor()
     cur.execute("\
@@ -179,78 +152,10 @@ def swiss_pairings():
     query_results = cur.fetchall()
     conn.close()
 
-    print query_results
     loop_amount = len(query_results) / 2
     paired_players = []
-    for n in range(loop_amount):
-        pair = query_results[0::2][n] + query_results[1::2][n]
+    for index in range(loop_amount):
+        pair = query_results[0::2][index] + query_results[1::2][index]
         paired_players.append(pair)
 
     return paired_players
-
-
-def enough_players():
-    """Checks if there are enough players to play the game"""
-    conn = connect()
-    cur = conn.cursor()
-    cur.execute("SELECT count(*) as player_count FROM players");
-    player_count = cur.fetchall()
-    conn.close()
-
-    print player_count[0][0]
-
-
-def set_rounds():
-
-    conn = connect()
-    cur = conn.cursor()
-    cur.execute("SELECT count(*) as player_count FROM players");
-    player_count = cur.fetchall()
-    conn.close()
-    return int(math.log(player_count[0][0], 2))
-
-
-def start_game():
-    """This functions starts the game"""
-
-    initialize_players()
-    #enough_players()
-    #rounds_amount = set_rounds()
-    #loop_through_rounds(rounds_amount)
-    player_standings()
-
-
-def loop_through_rounds(rounds_amount):
-    """Loops through all the matches"""
-
-    for n in range(rounds_amount):
-        paired_players = swiss_pairings()
-        loop_through_matches(paired_players)
-
-
-def loop_through_matches(paired_players):
-    """loops through all the matches in each round"""
-
-    for pair in paired_players:
-        selected_winner = bool(random.getrandbits(1))
-
-        if(selected_winner):
-            report_match(pair[0], pair[2])
-        else:
-            report_match(pair[2], pair[0])
-
-
-def start_matches():
-
-    for n in range(4):
-        print n
-
-
-def end_game():
-    """This function Ends the game"""
-    delete_players()
-    delete_matches()
-
-
-#start_game()
-#end_game()
