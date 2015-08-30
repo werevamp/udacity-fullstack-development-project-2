d0659a3c22de2b7dededff43d7e0c8eff395def5
Albert De Guzman <adeguzman@launchbrigade.com>
2015-08-30 15:41:57 -0700

    removing unused files

--- players.py
+++
@@ -1,8 +0,0 @@
-"""This file holds the player object"""
-
-
-class Player(object):
-    """Player is a class that contains player attributes"""
-    
-    def __init__(self, name):
-        self.name = name
